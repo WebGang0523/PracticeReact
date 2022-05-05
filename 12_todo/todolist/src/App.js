b91ff01e6991c0a73f2fe8fe191f5dc604c72591
xuchenggang <chenggang.xu@xipu.com>
2022-05-05 15:24:17 +0800

    todo静态组件

---
+++ 12_todo/todolist/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import List from './components/List'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List />
+          <Footer />
+        </div>
+      </div>
+    )
+  }
+}
