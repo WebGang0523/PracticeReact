120f6e661808c2366b5a282a474e0c4e579842db
xuchenggang <chenggang.xu@xipu.com>
2022-05-05 17:43:48 +0800

    动态初始话

--- 12_todo/todolist/src/App.js
+++ 12_todo/todolist/src/App.js
@@ -5,12 +5,20 @@ import Footer from './components/Footer'
 import List from './components/List'
 
 export default class App extends Component {
+  state = {
+    todos: [
+      { id: '001', name: '吃饭', done: true },
+      { id: '002', name: '睡觉', done: true },
+      { id: '003', name: '敲代码', done: false },
+    ],
+  }
   render() {
+    const { todos } = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header />
-          <List />
+          <List todos={todos} />
           <Footer />
         </div>
       </div>
--- 12_todo/todolist/src/components/Item/index.jsx
+++ 12_todo/todolist/src/components/Item/index.jsx
@@ -3,11 +3,12 @@ import './index.css'
 
 export default class Item extends Component {
   render() {
+    const { todo } = this.props
     return (
       <li>
         <label>
-          <input type="checkbox" />
-          <span>xxxxx</span>
+          <input type="checkbox" defaultChecked={todo.done} />
+          <span>{todo.name}</span>
         </label>
         <button className="btn btn-danger" style={{ display: 'none' }}>
           删除
