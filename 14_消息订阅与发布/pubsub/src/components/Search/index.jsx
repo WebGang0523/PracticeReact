2ce7a1a38d777471e8a44ffdd39b4bebae419e39
xuchenggang <chenggang.xu@xipu.com>
2022-05-07 20:32:12 +0800

    fetch

--- 14_消息订阅与发布/pubsub/src/components/Search/index.jsx
+++ 14_消息订阅与发布/pubsub/src/components/Search/index.jsx
@@ -3,29 +3,47 @@ import axios from 'axios'
 import PubSub from 'pubsub-js'
 
 export default class Search extends Component {
-  search = () => {
+  search = async () => {
     // 连续解构赋值并重命名
     const {
       keyWordElement: { value: keyword },
     } = this
 
     PubSub.publish('updateAppState', { isFirst: false, isLoading: true })
-    axios.get(`https://api.github.com/search/users?q=${keyword}`).then(
-      (response) => {
-        PubSub.publish('updateAppState', {
-          isLoading: false,
-          users: response.data.items,
-          err: '',
-        })
-      },
-      (err) => {
-        PubSub.publish('updateAppState', {
-          isLoading: false,
-          err: err.message,
-          users: [],
-        })
-      }
-    )
+    // axios.get(`https://api.github.com/search/users?q=${keyword}`).then(
+    //   (response) => {
+    //     PubSub.publish('updateAppState', {
+    //       isLoading: false,
+    //       users: response.data.items,
+    //       err: '',
+    //     })
+    //   },
+    //   (err) => {
+    //     PubSub.publish('updateAppState', {
+    //       isLoading: false,
+    //       err: err.message,
+    //       users: [],
+    //     })
+    //   }
+    // )
+    try {
+      const response = await fetch(
+        `https://api.github.com/search/users?q=${keyword}`
+      )
+      const result = await response.json()
+
+      PubSub.publish('updateAppState', {
+        isLoading: false,
+        users: result.items,
+        err: '',
+      })
+    } catch (err) {
+      PubSub.publish('updateAppState', {
+        isLoading: false,
+        err: err.message,
+        users: [],
+      })
+    }
   }
 
   render() {
